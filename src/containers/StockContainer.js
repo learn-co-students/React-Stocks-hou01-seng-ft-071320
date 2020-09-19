b6d4c9f173ccf921e5c11b02d3fa9d7347c63285
edgonzalez-9 <edgonzalezhernandez9@gmail.com>
2020-09-19 12:54:59 -0500

    Done.

--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import Stock from '../components/Stock'
 
 class StockContainer extends Component {
-
   render() {
+    const stocks = this.props.stocks
     return (
       <div>
         <h2>Stocks</h2>
         {
-          //render the list of stocks here
+          stocks.map(stock => <Stock key={stock.id} stock={stock} addToPortfolio={this.props.addToPortfolio}/>)
         }
       </div>
     );
