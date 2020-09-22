4b680108e85d4b36e4f994d272b5e94d0314c2d7
kirirotha <kirirotha@gmail.com>
2020-09-22 12:38:41 -0500

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,33 +1,64 @@
-import React from 'react';
-
-const SearchBar = () => {
-  return (
-    <div>
-
-      <strong>Sort by:</strong>
-      <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
-        Alphabetically
-      </label>
-      <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
-        Price
-      </label>
-      <br/>
-
-      <label>
-        <strong>Filter:</strong>
-        <select onChange={null}>
-          <option value="Tech">Tech</option>
-          <option value="Sportswear">Sportswear</option>
-          <option value="Finance">Finance</option>
-        </select>
-      </label>
+import React, { Component } from 'react';
 
 
-    </div>
-  );
+class SearchBar extends Component {
+  state={
+    sortby: "Alphabetically",
+    filter: "None"
+  }  
+  
+  updateSort = (e) =>{
+    //console.log(e.target.value)
+    this.setState({
+      ...this.state,
+      sortby: e.target.value
+    },
+    () =>{
+      this.props.handleSort(this.state.sortby)
+    })
+  }
+
+  updateFilter = (e) =>{
+    //console.log(e.target.value)
+    this.setState({
+      ...this.state,
+      filter: e.target.value
+    },
+    () =>{
+      this.props.handleFilter(this.state.filter)
+    })
+  }
+
+  render(){
+    return (
+
+      <div>
+
+        <strong>Sort by:</strong>
+        <label>
+          <input type="radio" name="sortby" value="Alphabetically" checked={null} onChange={this.updateSort}/>
+          Alphabetically
+        </label>
+        <label>
+          <input type="radio" name="sortby" value="Price" checked={null} onChange={this.updateSort}/>
+          Price
+        </label>
+        <br/>
+
+        <label>
+          <strong>Filter:</strong>
+          <select onChange={this.updateFilter} name='filter'>
+            <option value="None">None</option>
+            <option value="Tech">Tech</option>
+            <option value="Sportswear">Sportswear</option>
+            <option value="Finance">Finance</option>
+          </select>
+        </label>
+
+
+      </div>
+    );
+  }
 }
-
 
 export default SearchBar;
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -4,21 +4,116 @@ import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
+  state ={
+    stocks: [],
+    myStocks: [],
+    allStocks: [],
+    sort: ""
+  }
+
+
+  componentDidMount(){
+    fetch('http://localhost:3000/stocks')
+    .then(res => res.json())
+    .then(stocks =>{
+      this.setState(
+        {stocks: stocks,
+        allStocks: stocks}
+      )
+    })
+  }
+
+  addToPortfolio = (stock) => {
+    //console.log(stock)
+    let myPortfolio = this.state.myStocks
+    myPortfolio.push(stock)
+    this.setState({
+      ...this.state,
+      myStocks: myPortfolio
+    })
+  }
+
+  removeFromPortfolio = (stockToRemove) =>{
+    //console.log("click to remove!")
+    let myPortfolio = this.state.myStocks.filter(stock =>{
+      return stock !== stockToRemove
+    })
+    this.setState({
+      ...this.state,
+      myStocks: myPortfolio
+    })
+  }
+
+  handleSort = (sort) => {
+    this.setState({
+      ...this.state,
+      sort: sort
+    },
+    () => this.doSort()
+    )
+  }
+  
+  doSort = () => {
+    //console.log(sort)
+    if(this.state.sort === "Alphabetically"){
+      this.nameSort()
+    }else if(this.state.sort === "Price"){
+      this.priceSort()
+    }
+  }
+
+  priceSort = () =>{
+    let stockList = this.state.stocks.sort((a,b) =>{
+      return a.price - b.price
+    })
+    this.setState({
+      ...this.state,
+      stocks: stockList
+    })
+  }
+
+  nameSort = () => {
+    let stockList = this.state.stocks.sort((a,b) =>{
+      return a.name.localeCompare(b.name)
+    })
+    this.setState({
+      ...this.state,
+      stocks: stockList
+    })
+  }
+
+  handleFilter = (filter) => {
+    //console.log(filter)
+    let filteredStocks
+    if(filter === "None"){
+      filteredStocks = this.state.allStocks
+    }else{
+      filteredStocks = this.state.allStocks.filter(stock => {
+        return stock.type === filter
+      })
+    }
+    this.setState({
+      ...this.state,
+      stocks: filteredStocks
+    },
+    () => this.doSort()
+    )
+  }
 
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar handleSort={this.handleSort} handleFilter={this.handleFilter}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={this.state.stocks} addToPortfolio={this.addToPortfolio}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer myStocks={this.state.myStocks} removeFromPortfolio={this.removeFromPortfolio}/>
 
             </div>
           </div>
